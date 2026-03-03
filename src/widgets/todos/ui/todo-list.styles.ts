6bf74783c7efe7b285ff4e8249392d094e98b793
Даниил Тихонов <daniil.tihonov@lemanapro.ru>
2026-03-03 19:04:25 +0300

    fix: UI buttons micro fix

--- src/widgets/todos/ui/todo-list.styles.ts
+++ src/widgets/todos/ui/todo-list.styles.ts
@@ -13,7 +13,6 @@ export const FilterButton = styled.button<{ $active: boolean }>`
   cursor: pointer;
   background-color: ${(props) => (props.$active ? "#0487c4" : "#ffffff")};
   color: ${(props) => (props.$active ? "#fff" : "#0487c4")};
-  font-weight: ${(props) => (props.$active ? "bold" : "normal")};
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.05);
   transition:
     background-color 0.2s,
